b3cd0b3eb1d3198eb8001d7f1f3c26c6b5bf279c
Jason Bingham <jkbingh@sandia.gov>
2024-10-15 11:58:24 -0600

    feat: Add axis processing.

--- rtsim/axis.py
+++ rtsim/axis.py
@@ -3,6 +3,8 @@
 from .base import Base
 from .exceptions import FrameTypeError
 from .frame import Frame
+from .pva import ConstantPVA, TimePVA
+import numpy as np
 
 
 class Axis(Base):
@@ -27,3 +29,66 @@ class Axis(Base):
             raise FrameTypeError(var_name, "Fixed")
 
         self.zeta = zeta
+
+    def process(
+        self, mu: Frame, rho: Frame, alpha: ConstantPVA | TimePVA, omega: ConstantPVA | TimePVA
+    ) -> tuple[TimePVA, TimePVA]:
+        r"""
+        Process the axis to provide linear and angular PVA outputs of axis.
+
+        :param mu: Axis misalignment :math:`(\mu)` coordinate frame.
+        :type mu: Frame[Full]
+
+        :param rho: Axis rotating :math:`(\rho)` coordinate frame.
+        :type rho: Frame[Rotating]
+
+        :param alpha: Linear PVA from lower level axis.
+        :type alpha: ConstantPVA | TimePVA
+
+        :param omega: Angular PVA from lower level axis.
+        :type omega: ConstantPVA | TimePVA
+
+        :raises FrameTypeError: If 'mu' is not a Full type coordinate frame.
+        :raises FrameTypeError: If 'rho' is not a Rotating type coordinate frame.
+
+        :return: Linear and angular PVA from this axis.
+        :rtype: tuple[TimePVA, TimePVA]
+        """
+        if mu.frame_type != "Full":
+            var_name = "mu"
+            raise FrameTypeError(var_name, "Full")
+
+        self.mu = mu
+
+        if rho.frame_type != "Rotating":
+            var_name = "rho"
+            raise FrameTypeError(var_name, "Rotating")
+
+        self.rho = rho
+
+        C_dot_rho = self.rho.Omega @ self.rho.C
+        C_ddot_rho = self.rho.Omega_dot @ self.rho.C + self.rho.Omega @ self.rho.Omega @ self.rho.C
+
+        alpha_a = self.zeta.C @ (
+            self.mu.linear.a + C_ddot_rho @ alpha.p + 2 * C_dot_rho @ alpha.v + self.rho.C @ alpha.a
+        )
+
+        alpha_v = self.zeta.C @ (self.mu.linear.v + C_dot_rho @ alpha.p + self.rho.C @ alpha.v)
+
+        alpha_p = self.zeta.linear.p + self.zeta.C @ (self.mu.linear.p + self.rho.C @ alpha.p)
+
+        omega_a = self.zeta.C @ (
+            self.mu.angular.a
+            + self.mu.Omega @ self.mu.C @ self.rho.angular.v
+            + self.mu.C @ self.rho.angular.a
+            + self.mu.Omega @ self.mu.C @ self.rho.C @ omega.v
+            + self.mu.C @ self.rho.Omega @ self.rho.C @ omega.v
+            + self.mu.C @ self.rho.C @ omega.a
+        )
+
+        omega_v = self.zeta.C @ (self.mu.angular.v + self.mu.C @ self.rho.angular.v + self.mu.C @ self.rho.C @ omega.v)
+
+        return (
+            TimePVA(p=alpha_p, v=alpha_v, a=alpha_a),
+            TimePVA(p=np.zeros((omega_v.shape[0], 3, 1)), v=omega_v, a=omega_a),
+        )
--- test/test_axis.py
+++ test/test_axis.py
@@ -1,8 +1,9 @@
 """Axis component tests."""
 
+from math import tau
 import numpy as np
 import pytest
-from rtsim import Axis
+from rtsim import Axis, Body, Mount
 from rtsim import Frame
 from rtsim.exceptions import FrameTypeError
 from rtsim.pva import ConstantPVA, TimePVA
@@ -16,6 +17,40 @@ TLINEAR = TimePVA(p=GTI, v=GTI, a=GTI)
 
 CANGULAR = ConstantPVA(p=GCI, v=GCI, a=GCI)
 TANGULAR = TimePVA(p=GTI, v=GTI, a=GTI)
+
+MU = Frame(
+    linear=TimePVA(p=np.zeros((500, 3, 1)), v=np.zeros((500, 3, 1)), a=np.zeros((500, 3, 1))),
+    angular=TimePVA(p=np.zeros((500, 3, 1)), v=np.zeros((500, 3, 1)), a=np.zeros((500, 3, 1))),
+)
+
+RHO = Frame(
+    linear=ConstantPVA(p=np.zeros((3, 1)), v=np.zeros((3, 1)), a=np.zeros((3, 1))),
+    angular=TimePVA(p=np.zeros((500, 3, 1)), v=np.zeros((500, 3, 1)), a=np.zeros((500, 3, 1))),
+)
+
+BODY = Body(
+    "Generic",
+    Frame(
+        linear=ConstantPVA(p=np.array([[0.0], [0.0], [0.0508]]), v=np.zeros((3, 1)), a=np.zeros((3, 1))),
+        angular=ConstantPVA(p=np.array([[0.0], [tau / 2], [0.0]]), v=np.zeros((3, 1)), a=np.zeros((3, 1))),
+    ),
+)
+
+MOUNT = Mount(
+    "Generic",
+    Frame(
+        linear=ConstantPVA(p=np.array([[0.0], [0.0], [0.0254]]), v=np.zeros((3, 1)), a=np.zeros((3, 1))),
+        angular=ConstantPVA(p=np.zeros((3, 1)), v=np.zeros((3, 1)), a=np.zeros((3, 1))),
+    ),
+)
+
+ALPHA = ConstantPVA(
+    p=MOUNT.frame.linear.p + MOUNT.frame.C @ BODY.frame.linear.p,
+    v=np.zeros((3, 1)),
+    a=np.zeros((3, 1)),
+)
+
+OMEGA = ConstantPVA(p=np.zeros((3, 1)), v=np.zeros((3, 1)), a=np.zeros((3, 1)))
 
 
 def test_axis_exists():
@@ -40,3 +75,25 @@ def test_axis_full():
     """Test if Axis created with Full frame."""
     with pytest.raises(FrameTypeError):
         Axis("Generic", Frame(TLINEAR, TANGULAR))
+
+
+def test_process():
+    """Test axis processing."""
+    axis = Axis(
+        "SART Axis",
+        Frame(
+            linear=ConstantPVA(p=np.array([[0.0], [0.0], [-0.0762]]), v=np.zeros((3, 1)), a=np.zeros((3, 1))),
+            angular=ConstantPVA(p=np.array([[0.0], [tau / 2], [0.0]]), v=np.zeros((3, 1)), a=np.zeros((3, 1))),
+        ),
+    )
+
+    new_alpha, new_omega = axis.process(MU, RHO, ALPHA, OMEGA)
+
+    assert isinstance(new_alpha, TimePVA)
+    assert isinstance(new_omega, TimePVA)
+    assert (new_alpha.p == 0).all()
+    assert (new_alpha.v == 0).all()
+    assert (new_alpha.a == 0).all()
+    assert (new_omega.p == 0).all()
+    assert (new_omega.v == 0).all()
+    assert (new_omega.a == 0).all()
